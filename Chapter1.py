35fb02bbdaa1af85de92236b39f7041494ded2e5
ralicagulubova <rgylybova@melontech.com>
2014-02-18 18:15:00 +0200

    Chapter1

---
+++ Chapter1.py
@@ -0,0 +1,20 @@
+ trace=10/1.61
+ trace
+6.211180124223602
+timesec=43*60+30
+timesec
+2610
+ timepermile=timesec/trace
+ print 'timepermile'
+timepermile
+
+timepermile
+420.21000000000004
+ timepermilemin=timepermile/60
+print 'timepermilemin'
+timepermilemin
+7.003500000000001
+ asmileperhour=(1/timepermilemin)*60
+ asmileperhour
+8.567144998929106
+ 
