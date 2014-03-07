35faca3180a031f39ced5b0e8f19c7e1bb5ce901
ralicagulubova <rgylybova@melontech.com>
2014-03-07 09:20:14 +0200

    Chaptter7.py

---
+++ Chapter7.py/ex.1.py
@@ -0,0 +1,11 @@
+def square_root (a):
+    x = a / 2.0
+    epsilon = 0.00000001
+    while True:
+         print x
+         y = (x + a / x) / 2 
+         if y == x:
+              break 
+          
+      
+      square_root (7)
