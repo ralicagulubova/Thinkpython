b73d5f26a2f6d928bba686d002324fb13495b6fa
ralicagulubova <rgylybova@melontech.com>
2014-03-07 09:42:14 +0200

    Chapter17.py

---
+++ Chapter17.py/ex.py
@@ -0,0 +1,14 @@
+class Time(object):
+    
+    def __init__(self, hour = 0, minute = 0,second = 0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        
+        
+        
+        
+ 
+time = Time(4)
+    
+time.print_time()
